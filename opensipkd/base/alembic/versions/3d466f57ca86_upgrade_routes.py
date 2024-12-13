9907a29402d4f9c332a09a7ee9b4389b85af00ed
aa.gusti <aa.gusti@opensipkd.com>
2024-12-13 14:36:06 +0700

    perbaikan detable checkclick

---
+++ opensipkd/base/alembic/versions/3d466f57ca86_upgrade_routes.py
@@ -0,0 +1,24 @@
+"""upgrade routes
+
+Revision ID: 3d466f57ca86
+Revises: 8e703a7a0657
+Create Date: 2024-12-13 11:24:35.337079
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3d466f57ca86'
+down_revision = '8e703a7a0657'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
