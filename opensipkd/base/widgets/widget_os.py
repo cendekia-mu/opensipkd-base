f34bd454e3a889aed3119e6a722fe2ccc0ed7cef
aa.gustiana@gmail.com <aa.gustiana@gmail.com>
2025-10-21 19:29:21 +0700

    Refactor CaptchaWidget serialization to always generate captcha and simplify cstruct assignment

--- opensipkd/base/widgets/widget_os.py
+++ opensipkd/base/widgets/widget_os.py
@@ -378,11 +378,12 @@ class CaptchaWidget(Widget):
 
     def serialize(self, field, cstruct, **kw):
         file_name = ""
-        if not cstruct:
-            kode_captcha, file_name = img_captcha(self.request)
-            self.request.session["captcha"] = kode_captcha
+        # if not cstruct:
+        kode_captcha, file_name = img_captcha(self.request)
+        self.request.session["captcha"] = kode_captcha
 
-        cstruct = cstruct or self.url+file_name
+        # cstruct = cstruct or self.url+file_name
+        cstruct = self.url+file_name
         readonly = kw.get("readonly", self.readonly)
         template = readonly and self.readonly_template or self.template
         values = self.get_template_values(field, cstruct, kw)
