bcdc44cce334f761475e7d75fa846b55158d59d0
aa.gustiana@gmail.com <aa.gustiana@gmail.com>
2025-12-03 22:57:53 +0700

    Comment out RPC-related columns in Partner model

--- opensipkd/base/models/partner.py
+++ opensipkd/base/models/partner.py
@@ -78,10 +78,10 @@ class Partner(Base, PartnerModel):
     partner_files: Mapped["PartnerFiles"] = relationship(back_populates="partner")
 
     #todo alembic
-    rpc_url = Column(String(255))
-    rpc_user = Column(String(255))
-    rpc_callback = Column(String(255))
-    rpc_password = Column(String(255))
+    # rpc_url = Column(String(255))
+    # rpc_user = Column(String(255))
+    # rpc_callback = Column(String(255))
+    # rpc_password = Column(String(255))
 
     # npwp        = Column(String(16))
     # npwpd       = Column(String(16))
